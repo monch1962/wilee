85cb23c1b975d2d56ad84e4a4af6a146378e65f2
David Mitchell <monch1962@gmail.com>
2017-05-08 20:37:55 +1000

    General code tidy up

--- rest-test-runner.go
+++ rest-test-runner.go
@@ -10,72 +10,72 @@ import (
 	"time"
 )
 
-type Header struct {
+type header struct {
 	Header string `json:"header"`
 	Value  string `json:"value"`
 }
 
-type TestInfo struct {
-	Id          string `json:"id"`
+type testInfo struct {
+	ID          string `json:"id"`
 	Description string `json:"description"`
 	Version     string `json:"version"`
 	DateUpdated string `json:"date_uploaded"`
 	Author      string `json:"author"`
 }
 
-type Payload struct {
-	Headers []Header `json:"headers"`
+type payload struct {
+	Headers []header `json:"headers"`
 	Body    string   `json:"body"`
 }
 
-type Request struct {
+type request struct {
 	Verb    string  `json:"verb"`
-	Url     string  `json:"url"`
-	Payload Payload `json:"payload"`
+	URL     string  `json:"url"`
+	Payload payload `json:"payload"`
 }
 
-type Expect struct {
+type expect struct {
 	ParseAs      string      `json:"parse_as"`
-	HttpCode     int64       `json:"http_code"`
+	HTTPCode     int64       `json:"http_code"`
 	MaxLatencyMS int64       `json:"max_latency_ms"`
-	Headers      []Header    `json:"headers"`
+	Headers      []header    `json:"headers"`
 	Body         interface{} `json:"body"`
 }
 
-type Actual struct {
-	HttpCode  int             `json:"http_code"`
+type actual struct {
+	HTTPCode  int             `json:"http_code"`
 	LatencyMS int64           `json:"latency_ms"`
 	Headers   json.RawMessage `json:"headers"`
 	Body      json.RawMessage `json:"body"`
 }
 
-type TestResult struct {
+type testResult struct {
 	PassFail  string   `json:"pass_fail"`
 	Timestamp string   `json:"timestamp"`
-	TestInfo  TestInfo `json:"test_info"`
-	Request   Request  `json:"request"`
-	Expect    Expect   `json:"expect"`
-	Actual    Actual   `json:"actual"`
+	TestInfo  testInfo `json:"test_info"`
+	Request   request  `json:"request"`
+	Expect    expect   `json:"expect"`
+	Actual    actual   `json:"actual"`
 }
 
-type TestRequest struct {
-	TestInfo TestInfo `json:"test_info"`
-	Request  Request  `json:"request"`
-	Expect   Expect   `json:"expect"`
+type testRequest struct {
+	TestInfo testInfo `json:"test_info"`
+	Request  request  `json:"request"`
+	Expect   expect   `json:"expect"`
 }
 
-type TestInput struct {
-	testinfo TestInfo `json:"test_info"`
-}
+/*type TestInput struct {
+	testinfo TestInfo //`json:"test_info"`
+}*/
 
-func readTestJson() TestRequest {
+func readTestJSON() testRequest {
 	// Read JSON input from stdin and return as a formatted Go struct
 	j, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
 		log.Println("Error reading content from stdin")
 		panic(err)
 	}
-	var tr TestRequest
+	var tr testRequest
 	err = json.Unmarshal(j, &tr)
 	if err != nil {
 		log.Println("Error parsing content read from stdin")
@@ -86,23 +86,23 @@ func readTestJson() TestRequest {
 	return tr
 }
 
-func populateRequest(testCaseRequest TestRequest) (TestInfo, Request, Expect) {
-	testinfo := &TestInfo{
-		Id:          testCaseRequest.TestInfo.Id,
+func populateRequest(testCaseRequest testRequest) (testInfo, request, expect) {
+	testinfo := &testInfo{
+		ID:          testCaseRequest.TestInfo.ID,
 		Description: testCaseRequest.TestInfo.Description,
 		Version:     testCaseRequest.TestInfo.Version,
 		DateUpdated: testCaseRequest.TestInfo.DateUpdated,
 		Author:      testCaseRequest.TestInfo.Author,
 	}
 
-	request := &Request{
+	request := &request{
 		Verb: testCaseRequest.Request.Verb,
-		Url:  testCaseRequest.Request.Url,
+		URL:  testCaseRequest.Request.URL,
 	}
 
-	expect := &Expect{
+	expect := &expect{
 		ParseAs:      testCaseRequest.Expect.ParseAs,
-		HttpCode:     testCaseRequest.Expect.HttpCode,
+		HTTPCode:     testCaseRequest.Expect.HTTPCode,
 		MaxLatencyMS: testCaseRequest.Expect.MaxLatencyMS,
 		Headers:      testCaseRequest.Expect.Headers,
 		Body:         testCaseRequest.Expect.Body,
@@ -110,9 +110,9 @@ func populateRequest(testCaseRequest TestRequest) (TestInfo, Request, Expect) {
 	return *testinfo, *request, *expect
 }
 
-func executeRequest(request Request) (interface{}, interface{}, int, time.Duration) {
+func executeRequest(request request) (interface{}, interface{}, int, time.Duration) {
 	httpClient := &http.Client{}
-	req, err := http.NewRequest(request.Verb, request.Url, nil)
+	req, err := http.NewRequest(request.Verb, request.URL, nil)
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -123,7 +123,7 @@ func executeRequest(request Request) (interface{}, interface{}, int, time.Durati
 		log.Fatalln(err)
 	}
 	defer resp.Body.Close()
-	log.Printf("Response body\n%v\n", resp.Body)
+	//log.Printf("Response body\n%v\n", resp.Body)
 	responseDecoder := json.NewDecoder(resp.Body)
 	var v interface{} // Not sure what the response will look like, so just implement an interface
 	err = responseDecoder.Decode(&v)
@@ -137,14 +137,14 @@ func executeRequest(request Request) (interface{}, interface{}, int, time.Durati
 	return v, headers, httpCode, latency
 }
 
-func populateResponse(body interface{}, headers interface{}, httpCode int, latency time.Duration) Actual {
-	var actual Actual
-	actual.HttpCode = httpCode
+func populateResponse(body interface{}, headers interface{}, httpCode int, latency time.Duration) actual {
+	var actual actual
+	actual.HTTPCode = httpCode
 	actual.LatencyMS = int64(latency / time.Millisecond)
 
 	var bodyStr json.RawMessage
 	bodyStr, _ = json.Marshal(body)
-	log.Printf("bodyStr\n%v\n", string(bodyStr))
+	//log.Printf("bodyStr\n%v\n", string(bodyStr))
 	actual.Body = bodyStr
 
 	var headerStr json.RawMessage
@@ -153,27 +153,31 @@ func populateResponse(body interface{}, headers interface{}, httpCode int, laten
 
 	return actual
 }
-
-func compareActualVersusExpected(actual Actual, expect Expect) bool {
+func compareActualVersusExpected(actual actual, expect expect) bool {
 	// compare the actual response against the expected response, and return a
-	// boolean indicating whether the match is good or not
-	if expect.ParseAs == "regex" {
+	// boolean indicating whether the match is good or bad
+
+	switch expect.ParseAs {
+	case "":
+		// if there's no parser defined, return false; this is a viable approach
+		// for simply collecting info about an API response but not a test case
 		return false
+	case "regex":
+		return true
+	case "exact_match":
+		return false
+	case "partial_match":
+		return false
+	default:
+		panic("expect.parse_as should be one of 'regex', 'exact_match', 'partial_match'")
 	}
-	return true
 }
 
 func main() {
-	testCaseRequest := readTestJson()
-
-	log.Printf("testCase:\n%+v\n", testCaseRequest)
+	testCaseRequest := readTestJSON()
 
 	testinfo, request, expect := populateRequest(testCaseRequest)
-
 	body, headers, httpCode, latency := executeRequest(request)
-	log.Printf("Response body\n%v\n", body)
-	log.Printf("Response headers\n%v\n", headers)
-
 	actual := populateResponse(body, headers, httpCode, latency)
 
 	var passFail string
@@ -183,7 +187,7 @@ func main() {
 		passFail = "fail"
 	}
 
-	testresult := &TestResult{
+	testresult := &testResult{
 		PassFail:  passFail,
 		Timestamp: time.Now().Local().Format(time.RFC3339),
 		Request:   request,
