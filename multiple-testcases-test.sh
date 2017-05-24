d5054b7fb8d80a2cb070a779d4cce2c7de8641e9
David Mitchell <monch1962@gmail.com>
2017-05-24 22:47:21 +1000

    Added shell scripts to demo capability

---
+++ multiple-testcases-test.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+time APP="https://jsonplaceholder.typicode.com" TESTCASE="test-data/jsonplaceholder-test[0124]*.json" go run jtrunner.go
---
+++ single-testcase-test.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+time APP="https://jsonplaceholder.typicode.com" go run jtrunner.go < test-data/jsonplaceholder-test.json | jq
