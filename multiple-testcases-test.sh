fa39faa8e3053de8491a4f159e07e1a1438a1036
David Mitchell <monch1962@gmail.com>
2017-09-20 07:19:18 +1000

    Tweaked multiple test case

--- multiple-testcases-test.sh
+++ multiple-testcases-test.sh
@@ -3,6 +3,6 @@ rm test-data/*.result.json
 time APP="https://jsonplaceholder.typicode.com" TESTCASE="test-data/jsonplaceholder-test[0124]*.json" go run jtrunner.go
 for f in test-data/*.result.json 
 do
-	cat $f | jq '.pass_fail'
+	cat $f | jq '{result: .pass_fail, verb: .request.verb, url: .request.url }'
 done
 
