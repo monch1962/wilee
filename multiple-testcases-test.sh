d3253ede9dc54e3505159e43198d9d163084a3d8
David Mitchell <monch1962@gmail.com>
2017-09-19 14:48:21 +1000

    Fixed multiple-testcases-test.sh

--- multiple-testcases-test.sh
+++ multiple-testcases-test.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+rm test-data/*.result.json
 time APP="https://jsonplaceholder.typicode.com" TESTCASE="test-data/jsonplaceholder-test[0124]*.json" go run jtrunner.go
+for f in test-data/*.result.json 
+do
+	cat $f | jq '.pass_fail'
+done
+
