386fb8e3ca91ba69872123271e6d1d8656654032
David Mitchell <monch1962@gmail.com>
2017-09-20 10:59:45 +1000

    Moved stuff into demo/ directory

--- demo/multiple-testcases-test.sh
+++ demo/multiple-testcases-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm test-data/*.result.json
-time APP="https://jsonplaceholder.typicode.com" TESTCASE="test-data/jsonplaceholder-test[0124]*.json" go run jtrunner.go
+time APP="https://jsonplaceholder.typicode.com" TESTCASE="test-data/jsonplaceholder-test[0124]*.json" go run ../jtrunner.go
 for f in test-data/*.result.json 
 do
 	cat $f | jq '{result: .pass_fail, verb: .request.verb, url: .request.url }'
--- single-testcase-test.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-time APP="https://jsonplaceholder.typicode.com" go run jtrunner.go < test-data/jsonplaceholder-test.json | jq
