1d377e851f3fc8282727ca328dbf7c11ac942609
David Mitchell <monch1962@gmail.com>
2018-01-04 12:42:22 +1100

    Tidying up

--- demo/single-testcase-test.sh
+++ demo/single-testcase-test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-time APP="https://jsonplaceholder.typicode.com" go run ../jtrunner.go < test-data/jsonplaceholder-test.json | jq
+time APP="https://jsonplaceholder.typicode.com" go run ../wilee.go < test-data/jsonplaceholder-test.json | jq
