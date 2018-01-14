c4e1330d1d065af96a3b4e9ec85e513144f523d4
David Mitchell <monch1962@gmail.com>
2018-01-14 11:11:38 +1100

    Created offline demo environment

--- demo/005-multiple-testcases.sh
+++ demo/005-multiple-testcases.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm test-cases/*.result.json 2> /dev/null
-time APP="https://jsonplaceholder.typicode.com" TESTCASE="test-cases/jsonplaceholder-test[0124]*.json" go run ../wilee.go
+APP="http://localhost:51062" TESTCASE="test-cases/jsonplaceholder-test[012]*.json" go run ../wilee.go
 for f in test-cases/*.result.json
 do
-	cat $f | jq '{result: .pass_fail, verb: .request.verb, url: .request.url }' | jq --arg result "$f" '.test_result |= $result'
+	cat $f | jq '{result: .pass_fail, pass_fail_reason: .pass_fail_reason, verb: .request.verb, url: .request.url }' | jq --arg result "$f" '.test_result |= $result'
 done
