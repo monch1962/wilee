4684eaf7f48d9426156dab73dfc3d869444fb61c
David Mitchell <6182169+monch1962@users.noreply.github.com>
2018-05-30 12:30:52 +1000

    Updates to tests

--- wilee_test.go
+++ wilee_test.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"log"
 	"os"
 	"testing"
 )
 
 func TestLoadValidJSON(t *testing.T) {
-	testJSONfile := "test-data/jsonplaceholder-test.json"
+	testJSONfile := "demo/test-cases/jsonplaceholder-test.json"
 	fileHandle, err := os.Open(testJSONfile)
 	if err != nil {
 		t.Logf("test data file %v not found", testJSONfile)
@@ -19,6 +20,35 @@ func TestLoadValidJSON(t *testing.T) {
 	}
 }
 
-func TestLoadInvalidJSON(t *testing.T) {
-	t.Fail()
+func TestLoadBrokenJSON(t *testing.T) {
+	brokenJSONfile := "demo/test-cases/invalid/broken-json.json"
+	fileHandle, err := os.Open(brokenJSONfile)
+	if err != nil {
+		t.Logf("broken test data file %v not found", brokenJSONfile)
+		t.Fail()
+	}
+	_, err = readTestCaseJSON(fileHandle)
+	if err == nil {
+		t.Logf("Failed to detect broken JSON test file %v", brokenJSONfile)
+		t.Fail()
+	}
+}
+
+func TestLoadInvalidRequestJSON(t *testing.T) {
+	brokenJSONfile := "demo/test-cases/invalid/invalid-request-json.json"
+	fileHandle, err := os.Open(brokenJSONfile)
+	if err != nil {
+		t.Logf("broken test data file %v not found", brokenJSONfile)
+		t.Fail()
+	}
+	testcase, err := readTestCaseJSON(fileHandle)
+	if err != nil {
+		t.Logf("Unable to parse test data file %v as JSON", brokenJSONfile)
+		t.Fail()
+	}
+	log.Printf("testcase: %v", testcase.Request)
+	/*if testcase.Request != "" {
+		t.Logf("Test case file %v should have invalid request")
+		t.Fail()
+	}*/
 }
