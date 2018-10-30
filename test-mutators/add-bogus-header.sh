f001111b5085aa58076f99c28b9feb7017c1a2b1
Mitchell <davmitchell@AU10571.local>
2018-10-30 17:46:08 +1100

    Added test-mutator/add-bogus-header.sh

---
+++ test-mutators/add-bogus-header.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+RANDOM_HEADER_KEY=$(base64 -i /dev/urandom | fold -w 20 | head -1)
+RANDOM_HEADER_VALUE=$(base64 -i /dev/urandom | fold -w 20 | head -1)
+echo $RANDOM_HEADER_KEY
+echo $RANDOM_HEADER_VALUE
+
+echo $(cat) |
+   jq --arg RHK "$RANDOM_HEADER_KEY" --arg RHV "$RANDOM_HEADER_VALUE" '.request.headers[$RHK] = $RHV' |
+   jq '.expect.http_code=400' |
+   jq '.test_info.tags -= ["negative"]' |
+   jq '.test_info.tags += ["negative"]' |
+   jq '.test_info.tags += ["random_extra_header"]'
