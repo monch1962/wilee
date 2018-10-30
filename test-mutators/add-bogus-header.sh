2280d90b489270d71db50574a7649b134b3cfc32
Mitchell <davmitchell@AU10571.local>
2018-10-30 17:47:14 +1100

    Added test-mutator/add-bogus-header.sh

--- test-mutators/add-bogus-header.sh
+++ test-mutators/add-bogus-header.sh
@@ -3,8 +3,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 RANDOM_HEADER_KEY=$(base64 -i /dev/urandom | fold -w 20 | head -1)
 RANDOM_HEADER_VALUE=$(base64 -i /dev/urandom | fold -w 20 | head -1)
-echo $RANDOM_HEADER_KEY
-echo $RANDOM_HEADER_VALUE
+#echo $RANDOM_HEADER_KEY
+#echo $RANDOM_HEADER_VALUE
 
 echo $(cat) |
    jq --arg RHK "$RANDOM_HEADER_KEY" --arg RHV "$RANDOM_HEADER_VALUE" '.request.headers[$RHK] = $RHV' |
