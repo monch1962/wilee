416fab7ab13cd52848d5033299fba8df486192f2
Mitchell <davmitchell@AU10571.local>
2018-11-09 15:02:30 +1100

    Bug fix for test-mutators

--- test-mutators/append-bogus-header.sh
+++ test-mutators/append-bogus-header.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-RANDOM_HEADER_KEY=$(base64 -i /dev/urandom | fold -w 20 | head -1)
+#RANDOM_HEADER_KEY=$(base64 -i /dev/urandom | fold -w 20 | head -1)
+RANDOM_HEADER_KEY=$(dd if=/dev/random bs=10 count=1 2>/dev/null|od -An -tx1| tr -d ' \t\n')
 RANDOM_HEADER_VALUE=$(base64 -i /dev/urandom | fold -w 20 | head -1)
-echo $RANDOM_HEADER_KEY
-echo $RANDOM_HEADER_VALUE
+#echo $RANDOM_HEADER_KEY
+#echo $RANDOM_HEADER_VALUE
 
 echo $(cat) |
    jq --arg RHK "$RANDOM_HEADER_KEY" --arg RHV "$RANDOM_HEADER_VALUE" '.request.payload.headers += [{"header": ($RHK|tostring), "value": ($RHV|tostring)}]' |
--- test-mutators/append-huge-header.sh
+++ test-mutators/append-huge-header.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-RANDOM_HEADER_KEY=$(base64 -i /dev/urandom | fold -w 4096 | head -1)
+#RANDOM_HEADER_KEY=$(base64 -i /dev/urandom | fold -w 32 | head -1)
+RANDOM_HEADER_KEY=$(dd if=/dev/random bs=256 count=1 2>/dev/null|od -An -tx1| tr -d ' \t\n')
+
 RANDOM_HEADER_VALUE=$(base64 -i /dev/urandom | fold -w 4096 | head -1)
 #echo $RANDOM_HEADER_KEY
 #echo $RANDOM_HEADER_VALUE
