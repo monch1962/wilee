2f40c8ea4407d719ba5fd680eacf619606538806
David Mitchell <6182169+monch1962@users.noreply.github.com>
2018-11-22 22:26:44 +1100

    Update json2yaml.py

--- demo/json2yaml.py
+++ demo/json2yaml.py
@@ -3,4 +3,4 @@ import sys
 import json
 import yaml # need to 'pip install pyyaml' for this to work; 'brew install libyaml && sudo python -m easy_install pyyaml' on Mac
 
-print yaml.dump(yaml.load(json.dumps(json.loads(open(sys.argv[1]).read()))), default_flow_style=False)
+print(yaml.dump(yaml.load(json.dumps(json.loads(open(sys.argv[1]).read()))), default_flow_style=False))
